b5b39c0a3eb582b07d1cd995550a6619011f1659
Weverton do Couto Timoteo <weverton.ct@gmail.com>
2016-06-04 17:27:15 -0300

    Show score and give points on pickup squares

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,24 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerController : MonoBehaviour
 {
+    private const int TOTAL_PICK_UPS = 10;
+
     public float speed;
+    public Text countText;
+    public Text winText;
+
     private Rigidbody rb;
+    private int count;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        UpdateCountText();
+        winText.text = "";
     }
 
     void FixedUpdate()
@@ -25,6 +35,18 @@ public class PlayerController : MonoBehaviour
         if (other.gameObject.CompareTag("Pick Up"))
         {
             other.gameObject.SetActive(false);
+            count++;
+            UpdateCountText();
+
+            if (count >= TOTAL_PICK_UPS)
+            {
+                winText.text = "You win!";
+            }
         }
+    }
+
+    void UpdateCountText()
+    {
+        countText.text = "Count: " + count.ToString();
     }
 }
